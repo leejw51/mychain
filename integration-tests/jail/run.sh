7cd0153ff5f47b84c22ae93f8f4a6eb0217cbcbf
jongwhan lee <leejw51@gmail.com>
2019-11-19 22:07:27 +0800

    exit

--- integration-tests/jail/run.sh
+++ integration-tests/jail/run.sh
@@ -6,6 +6,10 @@ echo "docker compose ok"
 #echo "done"
 pip3 install docker
 python3 ./disk/jail_test.py
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit -1
+fi
 echo "test finished"
 sleep 4
 docker-compose down
