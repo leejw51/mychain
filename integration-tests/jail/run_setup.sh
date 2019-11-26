7b769747e032edb6dde194aa9d8aef3afb789cf4
jongwhan lee <jonghwan@crypto.com>
2019-11-27 01:29:47 +0800

    works

--- integration-tests/jail/run_setup.sh
+++ integration-tests/jail/run_setup.sh
@@ -3,6 +3,10 @@ echo "setup"
 sleep 1
 
 python3.7 ../bot/make.py
-cp ./node0/tendermint/config/* ./disk/config0/
-cp ./node1/tendermint/config/* ./disk/config1/
+cp ./node0/tendermint/config/genesis.json ./disk/config0/
+cp ./node0/tendermint/config/node_key.json ./disk/config0/
+cp ./node0/tendermint/config/priv_validator_key.json ./disk/config0/
 
+cp ./node1/tendermint/config/genesis.json ./disk/config1/
+cp ./node1/tendermint/config/node_key.json ./disk/config1/
+cp ./node1/tendermint/config/priv_validator_key.json ./disk/config1/
