a323323fd6570a237a000d02997fabd0dd53d3e7
Jongwhan Lee <jonghwan@crypto.com>
2019-12-10 14:59:24 +0800

    run_test

--- integration-tests/jail/run_test.sh
+++ integration-tests/jail/run_test.sh
@@ -10,4 +10,7 @@ echo "chain rpc port="$JAIL_CHAIN_RPC
 docker-compose up -d  
 echo "docker compose ok"
 nix-shell ./jail.nix  --run "export PASSPHRASE=1 && python3 ../bot/jail_test.py"
-
+ret=$?
+if [ $ret -ne 0 ]; then
+    exit -1
+fi
