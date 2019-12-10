9ab07d1e4fb73613763a9462f3094005fafcf9c7
leejw51 <leejw51@gmail.com>
2019-12-10 15:39:39 +0900

    test3

--- integration-tests/jail/run_test.sh
+++ integration-tests/jail/run_test.sh
@@ -10,13 +10,4 @@ echo "chain rpc port="$JAIL_CHAIN_RPC
 docker-compose up -d  
 echo "docker compose ok"
 nix-shell ./jail.nix  --run "export PASSPHRASE=1 && python3 ../bot/jail_test.py"
-ret=$?
-if [ $ret -ne 0 ]; then
-    docker-compose down
-    exit -1
-fi
-echo "test finished"
-sleep 4
-docker-compose down
-echo "OK"
-sleep 2
+
