be301ce6765470f71892e3239bc8f53cd4ba733f
Jongwhan Lee <jonghwan@crypto.com>
2019-12-12 16:04:33 +0800

    dummy shutdown

--- integration-tests/jail/shutdown.sh
+++ integration-tests/jail/shutdown.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+export JAIL_CLIENT_RPC=9981
+export JAIL_CHAIN_RPC=26657
+export APP_HASH=00000000
 export CURRENT_HASH=$(git rev-parse HEAD)
 echo "shutdown CURRENT_HASH=" $CURRENT_HASH
 docker-compose -p $CURRENT_HASH down
